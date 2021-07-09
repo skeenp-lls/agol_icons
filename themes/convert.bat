5cc920c5e65a23734c6cd2db6069fb7bb4580291
skeenp-lls <69334377+skeenp-lls@users.noreply.github.com>
2021-07-09 15:01:42 +1000

    Initial Commit

---
+++ themes/convert.bat
@@ -0,0 +1,4 @@
+rem FOR %%A IN (agri\*.svg) DO "C:\Apps\inkscape-1.1-x64\inkscape\bin\inkscape.exe" %%A --export-type=png --export-filename=%%A.png
+rem FOR %%A IN (utility\*.svg) DO "C:\Apps\inkscape-1.1-x64\inkscape\bin\inkscape.exe" %%A --export-type=png --export-filename=%%A.png
+rem FOR %%A IN (waterway\*.svg) DO "C:\Apps\inkscape-1.1-x64\inkscape\bin\inkscape.exe" %%A --export-type=png --export-filename=%%A.png
+FOR %%A IN (terrestrial\*.svg) DO "C:\Apps\inkscape-1.1-x64\inkscape\bin\inkscape.exe" %%A --export-type=png --export-filename=%%A.png
